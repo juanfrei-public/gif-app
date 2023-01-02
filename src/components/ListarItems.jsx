52d8456ae354d2b8a020b1729193cdc5269f72e4
Juan Manuel Bottarelli Frei <jbottarelliex@bice.cl>
2023-01-02 12:03:48 -0300

    avances del curso

--- src/components/ListarItems.jsx
+++ src/components/ListarItems.jsx
@@ -1,18 +1,16 @@
+
+import { useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+
 export const ListarItems = ({ items }) => {
-  const apiKey = "LOBcuGYUNa5QAVhlG1zCeOKCaFv5T7Qk";
-  const limit = 20;
 
-  const getGifs = async () => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${items}&limit=${limit}`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    console.log(data);
-  };
+  useEffect( () => {
+    getGifs(items);
 
-  getGifs();
+  }, [ ]);
+
 
   return (
-    
     <>
       <h3>{items}</h3>
     </>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+const apiKey = "LOBcuGYUNa5QAVhlG1zCeOKCaFv5T7Qk";
+const limit = 10;
+
+export const getGifs = async (items) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${items}&limit=${limit}`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+    console.log(gifs);
+    return gifs;
+};
