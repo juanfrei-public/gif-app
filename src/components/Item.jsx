5bc2653eac607d26f755626fd76f2f9c9d872cd6
Juan <juan.frei@globant.com>
2023-01-04 21:48:33 -0300

    test de componente item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,15 @@
+import PropTypes from "prop-types";
+
 export const Item = ({ title, url }) => {
   return (
     <div className="card">
       <img src={url} alt={title} />
-      <p> {title} </p>
+      <p>{title}</p>
     </div>
   );
+};
+
+Item.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
